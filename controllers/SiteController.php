913a6ec91ab2fa6ed8109ab5d67b36fadcd10e27
Tonis Ormisson <tonis@andmemasin.eu>
2018-03-23 15:10:00 +0200

    DEV ::className() -> ::class

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -25,7 +25,7 @@ class SiteController extends Controller
     {
         return [
             'access' => [
-                'class' => AccessControl::className(),
+                'class' => AccessControl::class,
                 'rules' => [
                     [
                         'actions' => ['index'],
