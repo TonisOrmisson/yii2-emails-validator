613a5e591d0f7a4e5a6fdd5f1561d28284b1929e
Tõnis Ormisson <tonis@andmemasin.eu>
2018-07-24 01:53:47 +0300

    initial test files

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace andmemasin\emailsvalidator;
+
+class ExampleTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \andmemasin\emailsvalidator\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
